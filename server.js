99339a10557f0b5628418215758b755cf5cde8f8
Eliza Regas <elizaregas@gmail.com>
2020-07-24 16:51:22 -0400

    home page now successfully linked

--- server.js
+++ server.js
@@ -10,11 +10,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("./Develop/public"));
 
+app.set('/', path.join(__dirname, '/Develop/public'));
+
+// app.get('/', (req, res) => {
+//   res.
+// })
 app.get('/exercise', (req, res) => {
-  res.sendFile(path.join(__dirname + '/Develop/public/exercise.html'));
+  res.sendFile(path.join(__dirname, '/Develop/public/exercise.html'));
 });
 
-// app.get("/exericse", ({ body }, res) => {
+// app.post("/", ({ body }, res) => {
 //   console.log(body);
 //   User.create(body)
 //     .then(dbUser => {
