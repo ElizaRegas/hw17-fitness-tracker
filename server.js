a2e3c887381d46d5ef03d78784e5fbbc777fcd7f
Eliza Regas <elizaregas@gmail.com>
2020-07-24 16:29:06 -0400

    mongoose set up and connected

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const logger = require("morgan");
-const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-// const User = require("./userModel.js");
 const app = express();
 
 app.use(logger("dev"));
@@ -13,12 +11,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("./Develop/public"));
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb");
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-mongoose.set('useUnifiedTopology', true);
 
 app.post("/submit", ({ body }, res) => {
   User.create(body)
