22aa2d2b04e974f6aa8dd65a17b1cbb09b460dcb
Eliza Regas <elizaregas@gmail.com>
2020-07-24 16:11:36 -0400

    serverjs hooked up and working

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+// const User = require("./userModel.js");
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("./Develop/public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb");
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
+app.post("/submit", ({ body }, res) => {
+  User.create(body)
+    .then(dbUser => {
+      res.json(dbUser);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
